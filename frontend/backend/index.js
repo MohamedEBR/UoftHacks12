057a9818d1e9eb889c5a1ee4bfac19cbdfcb48ea
MohamedEBR <110148669+MohamedEBR@users.noreply.github.com>
2025-01-18 00:29:30 -0500

    Image and video upload to db

--- frontend/backend/index.js
+++ frontend/backend/index.js
@@ -1,5 +1,34 @@
 const express = require("express");
+const multer = require('multer');
+const mongoose = require('mongoose');
+const File = require('./dbSchema.js');
+const dotenv = require('dotenv');
+const cors = require('cors');
+dotenv.config();
+
+const uri = process.env.URI;
+
+async function connectDB() {
+  try {
+    await mongoose.connect(uri);
+    console.log("Successfully connected to MongoDB with Mongoose!");
+  } catch (error) {
+    console.error("Error connecting to MongoDB:", error);
+  }
+}
+
+connectDB();
+
+
+
+
 const app = express();
+
+app.use(cors({
+  origin: "*",
+  methods: "GET,PUT,POST,DELETE",
+  credentials: true,
+}));
 
 // Middleware to parse JSON requests
 app.use(express.json());
@@ -9,8 +38,51 @@ app.get("/", (req, res) => {
   res.send("Hello, Express!");
 });
 
+// Configure multer for file uploads
+const upload = multer({
+  storage: multer.memoryStorage(),
+  limits: {
+    fileSize: 100000000 // 1MB
+  },
+  fileFilter(req, file, cb) {
+    if (!file.originalname.match(/\.(jpeg|jpg|JPG|png|pdf|doc|docx|xlsx|xls|mp4|avi|mov|mkv)$/)) {
+      return cb(
+        new Error(
+          'only upload files with jpg, jpeg, png, pdf, doc, docx, xlsx, xls format.'
+        )
+      );
+    }
+    cb(undefined, true); // continue with upload
+  }
+});
+
+// Handle file uploads
+app.post('/upload', (req, res, next) => {
+  upload.single('file')(req, res, (err) => {
+    if (err instanceof multer.MulterError) {
+      return res.status(400).send(err.message);
+    } else if (err) {
+      return res.status(400).send(err.message);
+    }
+    next();
+  });
+}, async (req, res) => {
+  try {
+    const { originalname, mimetype, buffer } = req.file;
+    const newFile = new File({
+      fileName: originalname,
+      mimeType: mimetype,
+      fileData: buffer,
+    });
+    await newFile.save();
+    res.status(201).send('File uploaded successfully');
+  } catch (error) {
+    console.error(error);
+    res.status(500).send('Error uploading file');
+  }
+});
 // Start the server
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT ;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
